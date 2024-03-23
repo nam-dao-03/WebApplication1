6435d040a8f154e5425f0cdf7d449c415f21fc23
nam-dao <namdao08042003@gmail.com>
2024-03-23 14:16:44 +0700

    asdfasdfasdf

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
